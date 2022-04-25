5f6e624bcacd5231558054d0b2204bf2ef02dfec
Kimberly <20197756@itla.edu.do>
2022-04-24 21:51:33 -0400

    update

--- pages/login_views/login_view.php
+++ pages/login_views/login_view.php
@@ -2,7 +2,7 @@
 <html>
     <head>
         <meta charset="UTF-8">
-        <title>Form Login | KimberlyDL</title>
+        <title>Login</title>
         <link rel="stylesheet" href="../../Css/login.css">
     </head>
 
@@ -10,20 +10,18 @@
         <div class="Login-box">
             <img class="icono" src="../../Img/Usuario.png" alt="Logo de Usario">
             <!-- <div>Iconos diseñados por <a href="https://www.flaticon.es/autores/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.es/" title="Flaticon">www.flaticon.es</a></div> -->
-            <h1>Login Here</h1>
+            <h1>Iniciar Sesión</h1>
             <form action="/FinalWeb/Services/login_services/login_service.php" method="POST">
                 <!--USERNAME-->  
-                <label for="username">Username</label>
-                <input type="text" name="username" placeholder="Enter Username">
+                <label for="username">Usuario</label>
+                <input type="text" name="username" placeholder="Ingrese su usuario">
 
                 <!--PASSWORD-->  
-                <label for="password">Password</label>
-                <input type="password" name="password" placeholder="Enter Password">
+                <label for="password">Clave</label>
+                <input type="password" name="password" placeholder="Ingrese su clave">
 
-                <input type="submit" name="login" value="Log In">
+                <input type="submit" name="login" value="Iniciar sesion">
 
-                <a href="#">Did you forget your password?</a><br>
-                <a href="#">Don't have an account?</a>
 
             </form>
         </div>
